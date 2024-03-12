ccf025d74a5f79d86a38f3436d418caeb23d2e4e
hiimluck3r <luck3rinc@yandex.ru>
2024-03-12 14:22:42 +0300

    fix: :bug: make flag check for initdb.sh entrypoint
    initdb entrypoints are fine as long as they're alive and restarted, however, Kubernetes doesn't operate with containers but with pods, so it will rewrite /data/db every single time pod is rerun. That commit fixes it (probably)

--- mongo/initdb.sh
+++ mongo/initdb.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+echo "check if golden file exists...";
 
-echo "Initializing default database...";
-mongorestore /app/mongo/dump/;
-echo "Entrypoint done";
+FILE=/data/db/golden
+
+if [ -f "$FILE" ]; then
+    echo "golden file exists, skipping installation...";
+else
+    echo "Initializing default database...";
+    mongorestore /app/mongo/dump/;
+    echo "Entrypoint done";
+    touch $FILE;
+    echo "golden file flag was created";
+fi
