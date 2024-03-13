82489198804d6f89daba16ce70bd1193c672d1fc
hiimluck3r <luck3rinc@yandex.ru>
2024-03-13 05:10:24 +0300

    fix: :wrench: provide link to download data dumps tar.gz

--- mongo/sharded-initdb.sh
+++ mongo/sharded-initdb.sh
@@ -6,7 +6,7 @@ FILE=/bitnami/mongodb/secrets/golden
 if [ -f "$FILE" ] || [ $MONGODB_SHARDING_MODE != "mongos" ]; then
     echo "golden file exists, skipping installation...";
 else
-    
+    curl -LO https://github.com/hiimluck3r/Midas/raw/backend/dump.tar.gz;
     tar -xzvf dump.tar.gz -C /;
     echo "Initializing default database...";
     mongorestore /dumps;
